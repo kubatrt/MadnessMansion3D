18b7b479280bc521b921ca02b6436e42e8740f3d
Jacob Storm <kubatrt@gmail.com>
2019-09-24 03:59:51 +0200

    Maze begining.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Maze mazePrefab;
+    public GameObject mazeObject;
+
+    private Maze mazeInstance;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BeginGame();
+
+        if (mazeObject == null) return;
+
+        Maze maze = mazeObject.GetComponent<Maze>();
+        if (maze) Debug.Log("Maze found!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Space pressed");
+        }
+
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            RestartGame();
+        }
+    }
+
+    private void BeginGame()
+    {
+        Debug.Log("Begin");
+
+        mazeInstance = Instantiate<Maze>(mazePrefab);
+    }
+
+    private void RestartGame()
+    {
+        Debug.Log("Restart");
+        Destroy(mazeInstance.gameObject);
+        BeginGame();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # MadnessMansion3D
-Unity FPS game project with random generated corridors.
+Unity horror theme FPS game stylized on oldschool classics shooters.
+
+
+
+Features
+* Random Maze-style generated levels
+* Minimap
+* Dark light and oil lamp
+* FPS camera and movement
+* Health and sanity statistics
+* Fight enemies
+* Melee and ranged weapons
+* Various enemies types
+* Simple inventory
+* Events and triggers
+
+Implementation
+* https://catlikecoding.com/unity/tutorials/maze/
